c3dbe324c1570ed67eb90d960908a75f7af025a1
Abhijit Barui <abhijitongit@gmail.com>
2021-09-18 03:26:30 +0530

    second commit settings/template pages app procedure

---
+++ pages/urls.py
@@ -0,0 +1,8 @@
+from os import name
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about', views.about,name='about'),
+]

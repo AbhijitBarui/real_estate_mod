a144c5b3dc66287363e9f888090d33f257c8fb46
Abhijit Barui <abhijitongit@gmail.com>
2021-09-18 19:34:20 +0530

    schema

--- real/settings.py
+++ real/settings.py
@@ -71,8 +71,11 @@ WSGI_APPLICATION = 'real.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'realestatedb',
+        'USER': 'postgres',
+        'PASSWORD': config('PASSWORD'),
+        'HOST': 'localhost'
     }
 }
 
